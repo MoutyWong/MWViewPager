157d70710cadc5c20a08f3f1fddabac871f99859
MoutyWong <mouty216@gmail.com>
2016-03-17 21:16:24 +0800

    Add image,create ScrollView

--- MWViewPager/MWViewPager/ViewController.m
+++ MWViewPager/MWViewPager/ViewController.m
@@ -9,17 +9,47 @@
 #import "ViewController.h"
 
 
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
 
-@interface ViewController ()
-
+@interface ViewController ()<UIScrollViewDelegate>
+@property (nonatomic,strong)UIScrollView * scroll;
+@property (nonatomic,strong)UIPageControl * pageCtl;
+@property (nonatomic,assign)NSInteger imgCount;
+@property (nonatomic,assign)NSInteger index;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.index = 0;
+    self.imgCount = 5;
+    [self createScrollView];
+    
 }
+
+- (void)createScrollView{
+    _scroll = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    _scroll.contentSize = CGSizeMake(kScreenWidth * 5, kScreenHeight);
+    for (NSInteger i = 0; i < 5; i++) {
+        UIImageView * imgView = [[UIImageView alloc]initWithFrame:CGRectMake(kScreenWidth * i, 0, kScreenWidth, kScreenHeight)];
+        NSString * imgName;
+        if (i == 0) {
+            imgName = [NSString stringWithFormat:@"%ld.jpg",i + 5];
+        }else{
+            imgName = [NSString stringWithFormat:@"%ld.jpg",i];
+        }
+        imgView.image = [UIImage imageNamed:imgName];
+        imgView.tag = 2015 + i;
+        [_scroll addSubview:imgView];
+    }
+    _scroll.contentOffset = CGPointMake(kScreenWidth, 0);
+    _scroll.pagingEnabled = YES;
+    _scroll.delegate = self;
+    [self.view addSubview:_scroll];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
